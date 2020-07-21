1f694d63bc11d167086fb708ea2fd1905e096f5e
andriislipenko <andrii.slipenko@gmail.com>
2020-07-21 10:58:09 +0300

    basic notes (CRUD and search)

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -1,13 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query, Body, Put, Delete } from '@nestjs/common';
 import { Note } from './schemas/note.schema';
 import { NotesService } from './notes.service';
+import { CreateNoteDto } from './dto/create-note.dto';
 
 @Controller('notes')
 export class NotesController {
     constructor(private notesService: NotesService) {}
     
     @Get()
-    findAll(): Promise<Note[]> {
+    findAllNotes(@Query('title') title: string): Promise<Note[]> {
+        if (title) return this.notesService.findByTitle(title);
+
         return this.notesService.findAll();
+    }
+
+    @Get(':id')
+    findNote(@Param('id') id: string): Promise<Note> {
+        return this.notesService.findById(id);
+    }
+
+    @Post()
+    createNote(@Body() createNoteDto: CreateNoteDto): Promise<Note> {
+        return this.notesService.create(createNoteDto);
+    }
+
+    @Put(':id')
+    updateNote(@Param('id') id: string, @Body() createNoteDto: CreateNoteDto): Promise<Note> {
+        return this.notesService.update(id, createNoteDto);
+    }
+
+    @Delete(':id')
+    deleteNote(@Param('id') id: string): Promise<any> {
+        return this.notesService.delete(id);
     }
 }
